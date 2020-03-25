ef4d172be63023896d3bb24147ea0108f1705013
przemyslaw.petka <przemyslaw.petka@schibsted.com>
2020-03-25 14:56:26 +0100

    adding logic that causes bug

--- app/src/main/java/com/ppetka/bufferingnotinsyncwithbuttons/MainActivity.kt
+++ app/src/main/java/com/ppetka/bufferingnotinsyncwithbuttons/MainActivity.kt
@@ -3,7 +3,10 @@ package com.ppetka.bufferingnotinsyncwithbuttons
 import android.annotation.SuppressLint
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.source.MediaSource
@@ -20,9 +23,12 @@ class MainActivity : AppCompatActivity() {
         findViewById<PlayerView>(R.id.playerView)
     }
 
+    private lateinit var pauseButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        pauseButton = playerView.findViewById(R.id.exo_pause)
         aspectRatioContainer.setAspectRatio(16f / 9)
 
         initializePlayer()
@@ -35,6 +41,17 @@ class MainActivity : AppCompatActivity() {
                 playWhenReady = true
             }
         playerView.player = player
+
+        player.addListener(object : Player.EventListener {
+            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+                if (playbackState == Player.STATE_BUFFERING) {
+                    pauseButton.visibility = View.GONE
+                } else {
+                    //here the logic would be different
+                    pauseButton.visibility = View.VISIBLE
+                }
+            }
+        })
 
         val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
             this,
