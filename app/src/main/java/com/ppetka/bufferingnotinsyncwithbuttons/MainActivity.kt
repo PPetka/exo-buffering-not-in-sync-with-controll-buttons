6448e2db6c104b65b7c9faa3c2fd013e1898df8a
przemyslaw.petka <przemyslaw.petka@schibsted.com>
2020-03-25 14:06:43 +0100

    base video is now playing

--- app/src/main/java/com/ppetka/bufferingnotinsyncwithbuttons/MainActivity.kt
+++ app/src/main/java/com/ppetka/bufferingnotinsyncwithbuttons/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.ppetka.bufferingnotinsyncwithbuttons
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val playerView: PlayerView by lazy {
+        findViewById<PlayerView>(R.id.playerView)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        aspectRatioContainer.setAspectRatio(16f / 9)
+
+        initializePlayer()
+    }
+
+    private fun initializePlayer() {
+        val player = SimpleExoPlayer.Builder(this)
+            .build()
+            .apply {
+                playWhenReady = true
+            }
+        playerView.player = player
+
+        val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
+            this,
+            Util.getUserAgent(this, "MyApplication2")
+        )
+        val videoSource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource((Uri.parse("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")))
+
+        player.prepare(videoSource)
     }
 }
